c978387ef408ae93fb9d3822f493407be7c564f3
Trevlin <152470186+loblachi@users.noreply.github.com>
2025-04-05 19:27:09 +0200

    Update db.js
    updated the db , removed the seed data

--- db.js
+++ db.js
@@ -6,30 +6,5 @@ export let eventsArr = [
         description: "Come and join us for the celebration. Bhajans to be rendered by Shakti Nagara. Prasad and offerings are welcome. Your presence will be highly appreciated.",
         isToday: false,
         hasPassed: false
-    },
-    {
-        name: "Hanuman Jayanthi",
-        date: "2025-04-01",
-        time: "6:00 PM",
-        description: "Come and join us for the celebration. Bhajans to be rendered by Shakti Nagara. Prasad and offerings are welcome. Your presence will be highly appreciated.",
-        isToday: false,
-        hasPassed: false
-    },
-    {
-        name: "Hanuman Jayanthi",
-        date: "2025-04-04",
-        time: "6:00 PM",
-        description: "Come and join us for the celebration. Bhajans to be rendered by Shakti Nagara. Prasad and offerings are welcome. Your presence will be highly appreciated.",
-        isToday: false,
-        hasPassed: false
-    },
-    {
-        name: "Hanuman Jayanthi",
-        date: "2025-04-20",
-        time: "6:00 PM",
-        description: "Come and join us for the celebration. Bhajans to be rendered by Shakti Nagara. Prasad and offerings are welcome. Your presence will be highly appreciated.",
-        isToday: false,
-        hasPassed: false
     }
-
 ]
